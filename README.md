24897597d52838738b227ee794a2d2d429845916
bonnyandsky <bonnyandsky@163.com>
2021-12-28 15:10:15 +0800

    新建

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# openwrt-v21_x86-64
- 基于官方原版编译
+[中文](https://p3terx.com/archives/build-openwrt-with-github-actions.html)
+
+./scripts/feeds update helloworld 
+
+./scripts/feeds install -a helloworld
+
+编译步骤：
+
+sudo apt-get update
+
+sudo sh -c "apt update && apt upgrade -y"
+
+sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
+
+sudo apt install ca-certificates
+
+git clone https://github.com/coolsnowwolf/lede
+
+cd lede
+
+lede\feeds.conf.default添加
+```bash
+src-git helloworld https://github.com/fw876/helloworld
+src-git lienol https://github.com/xiaorouji/openwrt-passwall
+```
+
+./scripts/feeds update -a
+
+./scripts/feeds install -a
+
+make menuconfig
+
+make -j8 download V=s 下载dl库（国内请尽量全局科学上网）
+
+find dl -size -1024c -exec ls -l {} \;  列出下载不完整的文件
+
+find dl -size -1024c -exec rm -f {} \;  删除不完整的文件
+
+make -j1 V=s （-j1 后面是线程数。第一次编译推荐用单线程）即可开始编译你要的固件了。
+
+二次编译：
+
+cd lede
+
+git pull
+
+./scripts/feeds update -a && ./scripts/feeds install -a
+
+make defconfig
+
+make -j8 download
+
+make -j$(($(nproc) + 1)) V=s
+
+如果需要重新配置：
+
+rm -rf ./tmp && rm -rf .config
+
+make menuconfig
+
+make -j$(($(nproc) + 1)) V=s
+
+编译完成后输出路径：bin/targets
---
+++ x86-64_mysetting-ipv6-2in1/diy_part2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+
+# 移除重复软件包
+rm -rf package/lean/luci-app-netdata
+rm -rf package/lean/luci-app-wrtbwmon
+rm -rf package/lean/luci-theme-argon
+
+# 添加额外软件包，不在根目录要用svn co，然后tree/main替换成trunk
+# git clone https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
+git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-advanced package/luci-app-advanced
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/luci-app-netdata
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netspeedtest package/luci-app-netspeedtest
+
+# 流量监控
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-wrtbwmon package/luci-app-wrtbwmon
+svn co https://github.com/sirpdboy/sirpdboy-package/trunk/wrtbwmon package/wrtbwmon
+
+# Themes
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+# 修改插件名字
+sed -i 's/"流量"/"实时流量监测"/g' `grep "流量" -rl ./`
+
+./scripts/feeds update -a
+./scripts/feeds install -a

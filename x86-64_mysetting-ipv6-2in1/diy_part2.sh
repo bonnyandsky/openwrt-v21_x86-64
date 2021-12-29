8ddfc6814dbb56470b39a63e7a8fcf417547e558
bonnyandsky <30990861+bonnyandsky@users.noreply.github.com>
2021-12-29 14:09:05 +0800

    arpbind

--- x86-64_mysetting-ipv6-2in1/diy_part2.sh
+++ x86-64_mysetting-ipv6-2in1/diy_part2.sh
@@ -23,7 +23,7 @@ git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-p
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-advanced package/luci-app-advanced
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/luci-app-netdata
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netspeedtest package/luci-app-netspeedtest
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind package/luci-app-arpbind
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind package/luci-app-arpbind
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot package/luci-app-autoreboot
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ipsec-vpnd package/luci-app-ipsec-vpnd
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ramfree package/luci-app-ramfree

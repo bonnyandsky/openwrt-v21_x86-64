f11e49ba3381bfdf06688e94aedbef0b53b0625f
bonnyandsky <30990861+bonnyandsky@users.noreply.github.com>
2021-12-29 16:25:16 +0800

    确认这些插件可以编译成功

--- x86-64_mysetting-ipv6-2in1/diy_part2.sh
+++ x86-64_mysetting-ipv6-2in1/diy_part2.sh
@@ -23,14 +23,14 @@ git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-p
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-advanced package/luci-app-advanced
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/luci-app-netdata
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netspeedtest package/luci-app-netspeedtest
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind package/luci-app-arpbind
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot package/luci-app-autoreboot
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ipsec-vpnd package/luci-app-ipsec-vpnd
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ramfree package/luci-app-ramfree
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-softethervpn package/luci-app-softethervpn
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/autocore package/autocore
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/automount package/automount
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper package/ipv6-helper
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind package/luci-app-arpbind
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot package/luci-app-autoreboot
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ipsec-vpnd package/luci-app-ipsec-vpnd
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ramfree package/luci-app-ramfree
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-softethervpn package/luci-app-softethervpn
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/autocore package/autocore
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/automount package/automount
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper package/ipv6-helper
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier package/luci-app-zerotier
 
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/autosamba package/autosamba

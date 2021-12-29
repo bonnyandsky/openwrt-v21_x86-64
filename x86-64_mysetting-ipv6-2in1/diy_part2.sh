e633c2f2640547ff64c0038916449d4e2bd8fd07
bonnyandsky <30990861+bonnyandsky@users.noreply.github.com>
2021-12-29 09:36:15 +0800

    Create diy_part2.sh

--- x86-64_mysetting-ipv6-2in1/diy_part2.sh
+++ x86-64_mysetting-ipv6-2in1/diy_part2.sh
@@ -20,22 +20,22 @@ rm -rf package/lean/luci-theme-argon
 
 # 添加额外软件包，不在根目录要用svn co，然后tree/main替换成trunk
 git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-advanced package/luci-app-advanced
+#svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-advanced package/luci-app-advanced
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/luci-app-netdata
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netspeedtest package/luci-app-netspeedtest
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind package/luci-app-arpbind
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot package/luci-app-autoreboot
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ipsec-vpnd package/luci-app-ipsec-vpnd
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ramfree package/luci-app-ramfree
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-turboacc package/luci-app-turboacc
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shortcut-fe package/shortcut-fe
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-softethervpn package/luci-app-softethervpn
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd package/luci-app-vlmcsd
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/autocore package/autocore
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/automount package/automount
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/autosamba package/autosamba
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper package/ipv6-helper
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier package/luci-app-zerotier
+#svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netspeedtest package/luci-app-netspeedtest
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind package/luci-app-arpbind
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-autoreboot package/luci-app-autoreboot
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ipsec-vpnd package/luci-app-ipsec-vpnd
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ramfree package/luci-app-ramfree
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-turboacc package/luci-app-turboacc
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shortcut-fe package/shortcut-fe
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-softethervpn package/luci-app-softethervpn
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd package/luci-app-vlmcsd
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/autocore package/autocore
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/automount package/automount
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/autosamba package/autosamba
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper package/ipv6-helper
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier package/luci-app-zerotier
 
 # 流量监控
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-wrtbwmon package/luci-app-wrtbwmon
